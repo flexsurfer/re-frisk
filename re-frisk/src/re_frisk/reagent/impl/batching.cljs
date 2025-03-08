7ff25b6c3bf889e6f6f0458f8160767dfdd08206
Franco Biasin <franconbiasin+github@gmail.com>
2025-03-08 15:01:30 -0300

    [Mar7 review] Avoid inlining reagent in batching and component nses

--- re-frisk/src/re_frisk/reagent/impl/batching.cljs
+++ re-frisk/src/re_frisk/reagent/impl/batching.cljs
@@ -1,13 +1,13 @@
 (ns re-frisk.reagent.impl.batching
   (:require
-   [re-frisk.inlined-deps.reagent.v1v2v0.reagent.impl.batching :as batching]
+   [reagent.impl.batching :as batching]
    [re-frame.trace :as trace :include-macros true]
    [clojure.string :as string]
    [re-frisk.inlined-deps.reagent.v1v2v0.reagent.impl.component :as component]))
 
 (def operation-name (memoize (fn [c] (last (string/split (component/component-name c) #" > ")))))
 
-(defonce original-next-tick re-frisk.inlined-deps.reagent.v1v2v0.reagent.impl.batching/next-tick)
+(defonce original-next-tick reagent.impl.batching/next-tick)
 
 (defn next-tick
   [f]
@@ -19,10 +19,10 @@
        {:op-type :raf}
        (f)
        (trace/with-trace {:op-type :raf-end})
-       (when (false? (.-scheduled? re-frisk.inlined-deps.reagent.v1v2v0.reagent.impl.batching/render-queue))
+       (when (false? (.-scheduled? reagent.impl.batching/render-queue))
          (trace/with-trace {:op-type :reagent/quiescent}))))))
 
-(defonce original-run-queue re-frisk.inlined-deps.reagent.v1v2v0.reagent.impl.batching/run-queue)
+(defonce original-run-queue reagent.impl.batching/run-queue)
 
 (defn run-queue [a]
   ;; sort components by mount order, to make sure parents
@@ -38,5 +38,5 @@
 
 (defn patch-next-tick
   []
-  (set! re-frisk.inlined-deps.reagent.v1v2v0.reagent.impl.batching/next-tick next-tick)
-  (set! re-frisk.inlined-deps.reagent.v1v2v0.reagent.impl.batching/run-queue run-queue))
+  (set! reagent.impl.batching/next-tick next-tick)
+  (set! reagent.impl.batching/run-queue run-queue))
